6f4777f6df390d6c78d8bff6a6318fd1f23edcff
Ali Sci <58806996+aliscie@users.noreply.github.com>
2022-10-01 15:07:53 +0300

    🔴 issues I should get pkg with index.js and index_bg.js in it

--- src/autodox_assets/index.js
+++ src/autodox_assets/index.js
@@ -1,19 +1,26 @@
-import { autodox } from "../declarations/autodox";
+import {autodox} from "../declarations/autodox";
+import {autodox_assets} from "../declarations/autodox_assets";
 
 document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await autodox.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+    // e.preventDefault();
+    // const button = e.target.querySelector("button");
+    //
+    // const name = document.getElementById("name").value.toString();
+    //
+    // button.setAttribute("disabled", true);
+    //
+    // // Interact with foo actor, calling the greet method
+    // const greeting = await autodox.greet(name);
+    //
+    // button.removeAttribute("disabled");
+    //
+    // document.getElementById("greeting").innerText = greeting;
+    return false;
 });
+
+
+// import './style.scss';
+
+// import("/Users/apple/Desktop/autodox/pkg").then(module => {
+//   module.run_app();
+// });
